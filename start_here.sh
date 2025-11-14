b9f030647ad4d624dd0366e7a0a838c3ccc39a46
Tan Doan <tdoan@joinhomebase.com>
2025-11-14 14:54:31 -0700

    Fix the script to install homebrew

--- start_here.sh
+++ start_here.sh
@@ -14,7 +14,7 @@ xcode-select --install
 # install homebrew if not already installed
 if ! command -v brew >/dev/null 2>&1; then
     echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/tty
 fi
 
 brew update
